2c893970152cc561cca283acd281efe44ac15496
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-03 09:14:09 +0100

    feat: characters askeds on exercise createds on Main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,31 @@
 package org.example;
 
+import org.example.personalhospital.Especialist;
 import org.example.personalhospital.Pacient;
+import org.example.personalhospital.PacientAmbulancia;
+import org.example.personalhospital.StudiantMedical;
+
+import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
         Pacient pacient1 = new Pacient("Juan", 1,
-                "Amputarle el brazo.");
+                "Antigripal 10 días");
         Pacient pacient2 = new Pacient("Irene", 25,
                 "Gastrectomía Vertical.");
-        Pacient pacient3 = new Pacient("Pedro", 36,
-                "Escayolarle el brazo izquierdo.");
-        Pacient pacient4 = new Pacient("Sara", 25,
-                "Operación de estetica tipo 2.");
+        PacientAmbulancia pambulancia1 = new PacientAmbulancia("Maria",
+                1, "Escayolarle el brazo.", 15);
+        PacientAmbulancia pambulancia2 = new PacientAmbulancia("José",
+                4, "", 15);
+        Especialist especialist1 = new Especialist(LocalDate.of(LocalDate.now().getYear(), 6, 1),
+                35, 76, "Traumatologo");
+        Especialist especialist2 = new Especialist(LocalDate.of(LocalDate.now().getYear(), 6, 1),
+                35, 76, "Cardiologo");
+        StudiantMedical studiantMedical1 = new StudiantMedical(50, 17,
+                "Segundo");
+        StudiantMedical studiantMedical2 = new StudiantMedical(50, 17,
+                "Tercero");
     }
 }
--- src/test/java/MedicalTest.java
+++ src/test/java/MedicalTest.java
@@ -2,10 +2,5 @@ import org.example.personalhospital.Especialist;
 
 import java.time.LocalDate;
 void main() {
-    Especialist especialist = new Especialist(
-            LocalDate.of(2026, 6, 1),
-            25,
-            42,
-            "Pediatra");
 
 }
